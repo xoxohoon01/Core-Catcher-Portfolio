ba3523db5a13e326d2ec1cb0eedf272fd4aa4bc3
BL4CK DoG <xoxohoon01@gmail.com>
2026-03-06 23:17:49 +0900

    Refactor: FlyingBullet 너프, GC 제거

--- Scripts/Projectiles/Raven/RavenAttackController.cs
+++ Scripts/Projectiles/Raven/RavenAttackController.cs
@@ -5,89 +5,49 @@ using UnityEngine;
 
 public class RavenAttackController : BulletController
 {
+    static readonly float[] angles1 = { 0f };
+    static readonly float[] angles2 = { -45f, 45f };
+    static readonly float[] angles3 = { 0f, -45f, 45f };
+
     protected override void CheckHit(UnitController target)
     {
         base.CheckHit(target);
 
         ObjectPoolManager.Instance.Spawn($"AudioObject", transform.position, Quaternion.identity).GetComponent<AudioObject>().PlayAudio($"Hit{Random.Range(1, 3)}", "Hit");
 
-        PlayerController player = PlayerManager.Instance.GetPlayer();
-
         int flyingBulletLevel = CardManager.Instance.artifactEffectLevel["FlyingBullet"];
-        if (flyingBulletLevel > 0 && flyingBulletLevel < 3)
-        {
-            Quaternion startRotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
-        }
-        else if (flyingBulletLevel >= 3 && flyingBulletLevel < 5)
-        {
-            Quaternion startRotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation * Quaternion.Euler(0, -45, 0)).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
 
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation * Quaternion.Euler(0, 45, 0)).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
-        }
-        else if (flyingBulletLevel == 5)
+        if (flyingBulletLevel > 0)
         {
+            PlayerController player = PlayerManager.Instance.GetPlayer();
+
+            ArtifactCardScriptableObject card = CardManager.Instance.GetArtifact("FlyingBullet");
+            float damage = player.status.damage * card.GetValue(AttributeType.amount, flyingBulletLevel);
+
             Quaternion startRotation = Quaternion.Euler(0, player.transform.eulerAngles.y, 0);
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
 
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation * Quaternion.Euler(0, -45, 0)).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
+            float[] angles =
+                flyingBulletLevel < 3 ? angles1 :
+                flyingBulletLevel < 5 ? angles2 :
+                angles3;
 
-            ObjectPoolManager.Instance.Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), startRotation * Quaternion.Euler(0, 45, 0)).GetComponent<FlyingBulletController>()
-                .Initialize(
-                (player.status.damage * 0.15f) + ((player.status.damage * 0.5f) * CardManager.Instance.artifactEffectLevel["FlyingBullet"]),
-                1,
-                80f,
-                10f,
-                false,
-                sender,
-                Faction.Player,
-                Vector3.one);
+            foreach (float angle in angles)
+            {
+                Quaternion rot = startRotation * Quaternion.Euler(0, angle, 0);
+
+                ObjectPoolManager.Instance
+                    .Spawn("FlyingBullet", player.transform.position + (Vector3.up * 2), rot)
+                    .GetComponent<FlyingBulletController>()
+                    .Initialize(
+                        damage,
+                        1,
+                        80f,
+                        10f,
+                        false,
+                        sender,
+                        Faction.Player,
+                        Vector3.one);
+            }
         }
 
         if (CardManager.Instance.artifactEffectLevel["Targeting"] > 0)
