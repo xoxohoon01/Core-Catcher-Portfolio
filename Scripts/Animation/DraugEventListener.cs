bb10538b05224bfd8834673c0eed0ebdb545ebb8
BL4CK DoG <xoxohoon01@gmail.com>
2026-03-13 01:00:16 +0900

    Feat: Indicator 쉐이더 텍스쳐 추가

--- Scripts/Animation/DraugEventListener.cs
+++ Scripts/Animation/DraugEventListener.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class DraugEventListener : MonoBehaviour
 {
+    public GameObject attackObject;
+
     public void StartAttack()
     {
         DraugController controller = GetComponent<DraugController>();
         controller.attackIndicator.gameObject.SetActive(true);
-        controller.indicator.gameObject.SetActive(true);
-        controller.indicator.Initialize(new Vector3(1, 1, 1), 0.5f, 0.05f, 20, 1f);
         controller.GetSuperArmor(1.1f);
 
+        Vector3 projectileSize = new Vector3(2, 2, 2);
         Quaternion startRotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
         Vector3 startPos = transform.position;
         Vector3 forwardPos = startRotation * Vector3.forward;
 
-        ObjectPoolManager.Instance.Spawn("Indicator", startPos + (forwardPos * 7f), startRotation)
-            .GetComponent<RoundIndicator>().Initialize(new Vector3(1, 1, 1), 0.5f, 0f, 360f, 1.15f);
-        ObjectPoolManager.Instance.Spawn("Indicator", startPos + (forwardPos * 14f), startRotation)
-            .GetComponent<RoundIndicator>().Initialize(new Vector3(1, 1, 1), 0.5f, 0f, 360f, 1.3f);
-        ObjectPoolManager.Instance.Spawn("Indicator", startPos + (forwardPos * 21f), startRotation)
-            .GetComponent<RoundIndicator>().Initialize(new Vector3(1, 1, 1), 0.5f, 0f, 360f, 1.45f);
+        ObjectPoolManager.Instance.Spawn("RoundIndicator", startPos + (forwardPos * 7f), startRotation)
+            .GetComponent<RoundIndicator>().Initialize(projectileSize, 0.5f, 0f, 360f, 1.15f);
+        ObjectPoolManager.Instance.Spawn("RoundIndicator", startPos + (forwardPos * 14f), startRotation)
+            .GetComponent<RoundIndicator>().Initialize(projectileSize, 0.5f, 0f, 360f, 1.3f);
+        ObjectPoolManager.Instance.Spawn("RoundIndicator", startPos + (forwardPos * 21f), startRotation)
+            .GetComponent<RoundIndicator>().Initialize(projectileSize, 0.5f, 0f, 360f, 1.45f);
     }
 
     public void Attack()
